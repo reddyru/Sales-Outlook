d075913a0b04551c141adb327fb02dcdb8bdb47e
Lin Lin Hlaing <linlinhlaing@Lins-Air.attlocal.net>
2024-03-16 23:43:20 -0400

    added file for Data Loading and Basic Analysis

---
+++ level_one.py
@@ -0,0 +1,29 @@
+# Level 1: Data Loading and Basic Analysis
+# 1. Load the sales dataset (e.g., 'sales.csv') into a Pandas DataFrame.
+# 2. Display the first 5 rows of the dataset.
+# 3. Check the shape (number of rows and columns) of the dataset.
+# 4. Display basic statistics (mean, median, min, max) for the 'Sales' column.
+# 5. Determine the number of unique products sold.
+
+
+import numpy as np
+import matplotlib as mat
+import pandas as pd
+
+salesData = pd.read_csv("csvFiles/sales.csv")
+
+print(salesData.head(6))
+print("Shape of dataset: ",salesData.shape)
+print("Mean of Sales amount: ",salesData['sales_amount'].mean())
+print("Median of Sales amount: ",salesData['sales_amount'].median())
+print("Min of Sales amount: ",salesData['sales_amount'].min())
+print("Max of Sales amount: ",salesData['sales_amount'].max())
+print("The number of unique products sold:",salesData['product_id'].nunique())
+
+print("testing==========")
+d = salesData.groupby(['sales_id','product_id']).size()
+print(d)
+column = ['sales_id','product_id',]
+d2 = salesData[column]
+# print(d2.loc[d2['product_id']>101])
+print(salesData[column][salesData['product_id']>104].reset_index(drop=True))
